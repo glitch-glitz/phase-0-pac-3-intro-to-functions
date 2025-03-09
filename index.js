fca45ed0b82a5c821384af48c24fbc1471efa424
glitch-glitz <fullstack.hub.devx@gmail.com>
2025-03-10 02:25:38 +0300

    Learned About Functions

--- index.js
+++ index.js
@@ -1,1 +1,70 @@
 // Follow along with the examples here
+
+//This is a function name (function keyword, function name, followed by parenthesis and curly brackets)
+function doNothing() {}
+//Bellow is a function declaration
+
+function sayHello() {
+  console.log("Hello!");
+}
+//For a function to function, it must be called out
+sayHello();
+
+function sayHelloToGuadalupe() {
+  console.log("Hello, Guadalupe!");
+}
+
+function sayHelloToLiz() {
+  console.log("Hello, Liz!");
+}
+
+function sayHelloToSamip() {
+  console.log("Hello, Samip!");
+}
+
+sayHelloToGuadalupe();
+sayHelloToLiz();
+sayHelloToSamip();
+
+//Understand arguments and parameters
+
+function doSomething(thing) {
+  console.log(thing); //the return value will be "anything"
+}
+
+doSomething("anything");
+
+function sayHelloTo(firstName) {
+  console.log(`Hello, ${firstName}!`);
+}
+
+sayHelloTo("Guadalupe"); // "Hello, Guadalupe!"
+sayHelloTo("Jane"); // "Hello, Jane!"
+sayHelloTo("R2-D2"); // "Hello, R2-D2!"
+sayHelloTo(1); // "Hello, 1!"
+
+//it worked
+function saySorryTo(neighbor) {
+  console.log(`Sorry, ${neighbor}!`);
+}
+saySorryTo("Faith");
+
+//Variable Scope
+/*1. Local scope
+2. Global scope 
+*/
+
+function say(greeting, firstName) {
+  console.log(`${greeting}, ${firstName}!`); //"Goodbye, Julio!"
+}
+say("Goodbye", "Julio");
+
+function say(greeting, lastName) {
+  console.log(`${greeting}, ${lastName}!`); //Ssup, Kwame!
+}
+say("Ssup", "Kwame");
+
+//Order of Arguments
+//the parameter names only have meaning to us, the programmer; JavaScript assigns values to parameters based solely on the order of the arguments that are passed.
+
+//Return Values in JavaScript
